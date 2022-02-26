5bb8a34899b8e455ab60c587043543145cf30637
Sarahi <al060884@uacam.mx>
2022-02-25 23:30:21 -0600

    se modifico la ventana principal de la aplicacion

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../public/img/favicon-16x16.png" type="image/x-icon">
-    <title>Consultorio</title>
+    <title>Biblioteca</title>
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     <!-- Compiled and minified CSS -->
--- public/home.php
+++ public/home.php
@@ -1,71 +1,77 @@
 <!-- Estilos en CSS -->
-<link rel="stylesheet" href="public/css/style_home.css">
+<link rel="stylesheet" href="public/css/style_hm.css">
 
 <!-- Contenido de la pagina principal -->
-<div class="row | header">
 
-    <div class="section | col s12 m8">
-        <!-- Note that "m4 l3" was added -->
-        <!-- Grey navigation panel -->
-        <h2 class="header__title">Bienvenido</h2>
-        <p class="header__content">Hola soy el Dr. Nombre Doctor, graduado de la 
-            Universidad Autónoma de Campeche sean bienvenidos a mi sitio donde podrán registrarse
-            y agendar una cita, para que su tiempo de espera sea la mínima y la atención la que usted
-            se merece.
-        </p>
-    </div>
-</div>
-<div class="container">
-    <div class="row">
+<div class="row"> 
+    <div class="col s8 espacio">
+      <div class="cotainer">
 
-        <div class="col s4">
-            <!-- Promo Content 1 goes here -->
-            <div class="card white darken-1 hoverable">
-                <div class="card-content black-text">
-                    <span class="card-title">Misión</span>
-                    <p>Brindar un servicio de calidad, donde los tiempos de espera sean los mínimos
-                        y proporcionarle información a la mano en cualquier lugar, pensando siempre 
-                        en su comodidad.</p>
+        <section class="black">
+            <div class="carousel carousel-slider" data-indicators="true">
+                <div class="carousel-fixed-item">
+                    <div class="container">
+                        <h1 class="black-text">Bienvenido
+                            
+                        </h1>
+
+                    </div>
                 </div>
-                <div class="card-action">
-                    <a href="#">Mas información</a>
-
+                <div class="carousel-item slime-item01   black-text" href="#one!">
+                    <div class="container">
+                        <h2>La Novia Sustituta </h2>
+                        <p class="black-text">LUCÍA DE AVILA.</p>
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="col s4">
-            <!-- Promo Content 2 goes here -->
-            <div class="card white darken-1 hoverable">
-                <div class="card-content black-text">
-                    <span class="card-title">Atención</span>
-                    <p>Te queremos brindar todos nuestros servicios con la mejor calidad, por eso
-                        nuestros horarios se ajustan a tus necesidades, te ofrecemos horarios de consulta
-                        flexibles.
-                    </p>
+                <div class="carousel-item  slime-item02 black-text" href="#two!">
+                    <div class="container">
+                        <h2>¡Protégete y apoya a los demás!</h2>
+                        <p class="black-text">Estemos unidos</p>
+                    </div>
                 </div>
-                <div class="card-action">
-                    <a href="#">Mas información</a>
-
+                <div class="carousel-item  slime-item03 black-text" href="#three!">
+                    <div class="container">
+                        <h2>#Yo me quedo en casa</h2>
+                        <p class="black-text">Cuida a tu familia.</p>
+                    </div>
+                </div>
+                <div class="carousel-item  slime-item04  white-text" href="#four!">
+                    <div class="container">
+                        <h2>¡Agenda tu cita desde aquí!</h2>
+                        <p class="black-text">Evita convivir con mucha gente</p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col s4">
-            <!-- Promo Content 3 goes here -->
-            <div class="card white darken-1 hoverable">
-                <div class="card-content black-text">
-                    <span class="card-title">Visión</span>
-                    <p>Proporcionarte un servicio de calidad que se ajuste a las nuevas regulaciones de
-                        salud y garanticen tu seguridad incluso antes de salir de casa, creando así
-                        espacios adecuados para ti y todos.
-                    </p>
-                </div>
-                <div class="card-action">
-                    <a href="#">Mas información</a>
-
-                </div>
-            </div>
-        </div>
-
-    </div>
+        </section>
 </div>
 
+
+
+    </div>
+    <div class="col s4">1</div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.1/js/materialize.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+
+<script>
+    // CAROUSEL
+    $(document).ready(function () {
+        $('.carousel').carousel(
+            {
+                dist: 0,
+                padding: 0,
+                fullWidth: true,
+                indicators: true,
+                duration: 50,
+            }
+        );
+    });
+
+    autoplay()
+    function autoplay() {
+        $('.carousel').carousel('next');
+        setTimeout(autoplay, 3000);
+    }
+</script>
