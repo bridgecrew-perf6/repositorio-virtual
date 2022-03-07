38294200f721e7387b94d7f2accef6b9400851e0
Reyna <al060872@uacam.mx>
2022-03-07 00:17:50 -0600

    Se modifico la pagina de inicio

--- public/home.php
+++ public/home.php
@@ -5,19 +5,19 @@
     $sql = $conexion->obtenerDatos($consulta);
 ?>
 <!-- Estilos en CSS -->
-<link rel="stylesheet" href="public/css/a.css">
+<link rel="stylesheet" href="public/css/b.css">
 
 <!-- Contenido de la pagina principal -->
 
-<div class="row | content">
-    <div class="col s8 espacio">
-        
+<div class="container">
+    <div class="row | content" >
 
+        <div class="col s12 m8 espacio">
             <section class="ventana">
                 <div class="carousel carousel-slider" data-indicators="true">
                     <div class="carousel-fixed-item">
                         <div class="container">
-                            <h2 class="amber lighten-1-text | titulo | center-align ">Estrenos mas recientes</h2>
+                            <h2 class="white-text indigo darken-4 | titulo | center-align ">Estrenos mas recientes</h2>
                         </div>
                     </div>
                     <div class="carousel-item slime-item01   black-text" href="#one!">
@@ -46,152 +46,127 @@
                     </div>
                 </div>
             </section>
-        
-        <div class="row">
-            <div class="col s12 ">
-                <div class="card-panel ">
-                    <h1>Autores mas populares</h1>
-                    <div class="divider"></div>
-                        <div class="section">
-                            <h5>Section 1</h5>
-                            <p>Stuff</p>
-                        </div>
+            
+            <div class="row">
+                <div class="col s12 ">
+                    
+                        <h3>Autores mas populares</h3>
                         <div class="divider"></div>
-                        <div class="section">
-                            <h5>Section 2</h5>
-                            <p>Stuff</p>
-                        </div>
-                        <div class="divider"></div>
-                        <div class="section">
-                            <h5>Section 3</h5>
-                            <p>Stuff</p>
-                        </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="col s4 | grey lighten-3 | panel-der">
-        <div class="row">
-
-            <h5 class="center-align">Destacados de la semana</h5>
-
-        </div>
-        
-        <div class="row">
-            <div class="col s12 ">
-
-                <div class="card horizontal ">
-                    <div class="card-image">
-                        <img class="responsive-img"
-                            src="./libros/portadas/<?php echo $sql[0]['codigoArchivo']; ?>.jpg">
-                    </div>
-                    <div class="card-stacked">
-                        <div class="card-action" >
-                            <a class="blue-text " href="?menu=lectorPdf">Novela</a>
-                        </div>
-                        <div class="card-content">
-                            <span class="new badge">4</span>Calificación</a>
-                            <br>
-                            <span><i class="material-icons">visibility</i></span>
+                            <div class="section">
+                                <h5>Autor 1</h5>
+                                <p>Stuff</p>
                             </div>
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col s12 ">
-
-                <div class="card horizontal ">
-                    <div class="card-image">
-                        <img class="responsive-img"
-                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSL7ZOxNnHJMVUtqr0RKddmzxIdw5hTt2rGCQ&usqp=CAU">
-                    </div>
-                    <div class="card-stacked">
-                        <div class="card-action">
-                            <a class="blue-text " href="#">Novela</a>
-                        </div>
-                        <div class="card-content">
-                            <span class="new badge">4</span>Calificación</a>
-                            <br>
-                            <span><i class="material-icons">visibility</i></span>
+                            <div class="divider"></div>
+                            <div class="section">
+                                <h5>Autor 2</h5>
+                                <p>Stuff</p>
                             </div>
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col s12 ">
-
-                <div class="card horizontal ">
-                    <div class="card-image">
-                        <img class="responsive-img"
-                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSL7ZOxNnHJMVUtqr0RKddmzxIdw5hTt2rGCQ&usqp=CAU">
-                    </div>
-                    <div class="card-stacked">
-                        <div class="card-action">
-                            <a class="blue-text " href="#">Novela</a>
-                        </div>
-                        <div class="card-content">
-                            <span class="new badge">4</span>Calificación</a>
-                            <br>
-                            <span><i class="material-icons">visibility</i></span>
+                            <div class="divider"></div>
+                            <div class="section">
+                                <h5>Autor 3</h5>
+                                <p>Stuff</p>
                             </div>
-                        
-                    </div>
+                    
                 </div>
             </div>
+       
         </div>
-        <div class="row">
-            <div class="col s12 ">
-
-                <div class="card horizontal ">
-                    <div class="card-image">
-                        <img class="responsive-img"
-                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSL7ZOxNnHJMVUtqr0RKddmzxIdw5hTt2rGCQ&usqp=CAU">
-                    </div>
-                    <div class="card-stacked">
-                        <div class="card-action">
-                            <a class="blue-text " href="#">Novela</a>
+        <!-- Lado Derecho -  Top´s -->
+        <div class="col s12 m4 ">
+            <div class="card card-panel" >
+                <div class="row">
+                    <h5 class="center-align | top-tl">Lo mas destacado</h5>
+                </div>
+                <div class="card hoverable">
+                    <div class="row | top" >
+                        <div class="col s3" >
+                            <h4 class="center-align | titulo">#01</h4>
                         </div>
-                        <div class="card-content">
-                            <span class="new badge red">4</span>Calificación</a>
+                        <div class="col s9" >
+                            <span class="titulo-nvl">Titulo de la lectura</span>
                             <br>
-                            <span><i class="material-icons">visibility</i></span>
-                            </div>
-                        
+                            <a href="#!" class="collection-item"><span class="new badge">000</span>Visitas</a>
+                            <br>
+                            <label for="">Calificación</label>
+                        </div>
+                    </div> 
+                </div>
+                <div class="card hoverable">
+                    <div class="row | top">
+                        <div class="col s3">
+                            <h4 class="center-align | titulo">#02</h4>
+                        </div>
+                        <div class="col s9">
+                            <span class="titulo-nvl">Titulo de la lectura</span >
+                            <p>Visitas 000K</p>
+                            <label for="">Calificación</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="card hoverable">
+                    <div class="row | top">
+                        <div class="col s3">
+                            <h4 class="center-align | titulo">#03</h4>
+                        </div>
+                        <div class="col s9">
+                            <span class="titulo-nvl">Titulo de la lectura</span>
+                            <p>Visitas 000K</p>
+                            <label for="">Calificación</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="card hoverable">
+                    <div class="row | top">
+                        <div class="col s3">
+                            <h4 class="center-align | titulo">#04</h4>
+                        </div>
+                        <div class="col s9">
+                            <span class="titulo-nvl">Titulo de la lectura</span>
+                            <p>Visitas 000K</p>
+                            <label for="">Calificación</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="card hoverable">
+                    <div class="row | top">
+                        <div class="col s3">
+                            <h4 class="center-align | titulo">#05</h4>
+                        </div>
+                        <div class="col s9">
+                            <span class="titulo-nvl">Titulo de la lectura</span>
+                            <p>Visitas 000K</p>
+                            <label for="">Calificación</label>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
 </div>
-    <!-- Scripts -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.1/js/materialize.min.js"></script>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 
-    <script>
-        // CAROUSEL
-        $(document).ready(function () {
-            $('.carousel').carousel(
-                {
-                    dist: 0,
-                    padding: 0,
-                    fullWidth: true,
-                    indicators: true,
-                    duration: 50,
-                }
-            );
-        });
+<!-- Scripts -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.1/js/materialize.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 
-        autoplay()
-        function autoplay() {
-            $('.carousel').carousel('next');
-            setTimeout(autoplay, 3000);
-        }
-    </script>
+<script>
+    // CAROUSEL
+    $(document).ready(function () {
+        $('.carousel').carousel(
+            {
+                dist: 0,
+                padding: 0,
+                fullWidth: true,
+                indicators: true,
+                duration: 50,
+            }
+        );
+    });
+
+    autoplay()
+    function autoplay() {
+        $('.carousel').carousel('next');
+        setTimeout(autoplay, 3000);
+    }
+</script>
